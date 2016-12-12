8a710002a5d90789ce0a503463b02f10b6b0f7f2
Rastislav Szabo <raszabo@cisco.com>
2016-12-12 16:07:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+[![Build Status](https://travis-ci.org/rastislavszabo/sysrepo-netopeer2-smoketests.svg?branch=master)](https://travis-ci.org/rastislavszabo/sysrepo-netopeer2-smoketests)
+
+# Sysrepo - Netopeer2 smoke tests
+
+Basic smoke tests for [Sysrepo](https://github.com/sysrepo/sysrepo) & [Netopeer2](https://github.com/CESNET/Netopeer2) based on [ncclient](https://github.com/ncclient/ncclient). The tests can be executed individually as a python script, e.g.:
+```
+python test_basic_operations.py
+```
+or altogether as a whole testsuite by executing:
+```
+python -m unittest discover -v
+```
---
+++ tests/test_basic_operations.py
@@ -0,0 +1,35 @@
+import unittest
+import nctest
+
+class TestBasicOperations(nctest.NCTestCase):
+    """ Tests basic NETCONF operations with no prerequisites on datastore content. """
+
+    def test_capabilities(self):
+        self.assertTrue(":startup" in self.nc.server_capabilities)
+        self.assertTrue(":candidate" in self.nc.server_capabilities)
+        self.assertTrue(":validate" in self.nc.server_capabilities)
+        self.assertTrue(":xpath" in self.nc.server_capabilities)
+
+    def test_get(self):
+        reply = self.nc.get()
+        self.check_reply_data(reply)
+
+    def test_get_config_startup(self):
+        reply = self.nc.get_config(source='startup')
+        self.check_reply_data(reply)
+
+    def test_get_config_running(self):
+        reply = self.nc.get_config(source='running')
+        self.check_reply_data(reply)
+
+    @unittest.skip("https://github.com/sysrepo/sysrepo/issues/600")
+    def test_copy_config(self):
+        reply = self.nc.copy_config(source='startup', target='candidate')
+        self.check_reply_ok(reply)
+
+    def test_neg_filter(self):
+        reply = self.nc.get(filter=("xpath", "/non-existing-module:non-existing-data"))
+        self.check_reply_err(reply)
+
+if __name__ == '__main__':
+    unittest.main()
